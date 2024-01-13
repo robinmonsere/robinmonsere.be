1ab042ab9e8f90d4531df0529fd848ce568f587d
Robin Monseré <robin.monsere@student.howest.be>
2024-01-13 17:59:11 +0100

    adding skillssets

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <div>
-    <button>
-        <p>View my blog</p>
-    </button>
+    <div class="button diagonal-tl-br-out">
+        <a href="">View my blog</a>
+    </div>
     <nav>
         <ul>
             <li><a href="/#about">About</a></li>
@@ -25,35 +25,63 @@
 
 
 <style>
+    .button {
+        margin-bottom: 2rem;
+        color: white;
+        background: none;
+        width: 10rem;
+        height: 2.5rem;
+        border: 2px white solid;
+        border-radius: 0.4rem;
+        text-align: center;
+        overflow: hidden;
+        position: relative;
+        transition: 1s ease;
+        z-index: 1;
+    }
+    .button:hover {
+        color: var(--color-bg-0);
+        border: 2px var(--color-bg-0) solid;
+    }
+
+    .button:before, .button:after {
+        transition: 0.5s ease;
+        content: '';
+        position: absolute;
+        z-index: -1;
+    }
+
+    .button a {
+        text-align: center;
+        padding-top: 0.7rem;
+    }
+
+    .diagonal-tl-br-out {
+        background: var(--color-theme-1);
+    }
+    .diagonal-tl-br-out:before, .diagonal-tl-br-out:after {
+        background: var(--color-bg-0);
+        top: 0;
+        bottom: 0;
+        transform: skewX(-45deg);
+        width: 175%;
+    }
+    .diagonal-tl-br-out:before {
+        left: -120%;
+    }
+    .diagonal-tl-br-out:after {
+        right: -120%;
+    }
+    .diagonal-tl-br-out:hover:before, .diagonal-tl-br-out:hover:after {
+        width: 105%;
+    }
+
 
     div {
         margin-top: 1rem;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
-    }
-
-    button {
-        margin-bottom: 2rem;
-        color: white;
-        background: none;
-        width: 10rem;
-        height: 2rem;
-        border: 1px white solid;
-        border-radius: 0.2rem;
-
-    }
-
-    button:hover {
-        background: white;
-        color: black;
-    }
-
-    .corner img {
-        color: white;
-        width: 5rem;
-        height: 5rem;
-        object-fit: contain;
     }
 
     ul {
@@ -86,11 +114,10 @@
         font-size: 0.8rem;
         text-transform: uppercase;
         letter-spacing: 0.1em;
-        text-decoration: none;
         transition: color 0.2s linear;
     }
 
-    a:hover {
+    nav a:hover {
         color: var(--color-theme-1);
     }
 </style>
