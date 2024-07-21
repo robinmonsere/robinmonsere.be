ba7cfda10f4d84d3394163e4a7a11345e1bee5ed
Robin Monseré <51537026+robinmonsere@users.noreply.github.com>
2024-07-21 10:36:51 +0200

    new site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 <p>I'm a software engineer with interest in spaceflight.</p>
 
 <p>You can email me at <a href="mailto:mail@robinmonsere.be">mail@robinmonsere.be</a></p>
-<p><a href="mailto:someone@example.com">Send email</a></p>
 
 <ul>
     <li>Here's my <a href="https://github.com/robinmonsere">Github</a></li>
